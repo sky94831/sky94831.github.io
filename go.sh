3c6bb0ecdceb925edcd972cc3d0eab421241a682
sky94831 <ppag0510a57a25f2@sohu.com>
2016-04-12 16:57:46 +0800

    新增上传脚本

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "ppag0510a57a25f2@sohu.com"
+git config --global user.name "sky94831"
+git config --global push.default simple
+git add .
+git commit -m $1
+git push
+start http://sky94831.github.io
